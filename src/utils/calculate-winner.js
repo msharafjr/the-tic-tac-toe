e13e01edc93d34717586617641d09fc9700b22d3
Mohamed Sharaf Jr <mohamedsharafjr@gmail.com>
2020-06-01 01:36:05 +0200

    add calculateWinner utility

---
+++ src/utils/calculate-winner.js
@@ -0,0 +1,25 @@
+/**
+ * Calculate the winner
+ * @param {Array[number]} board The board array
+ * @returns {Boolean}
+ */
+export default function calculateWinner(board) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return true;
+    }
+  }
+  return false;
+}
